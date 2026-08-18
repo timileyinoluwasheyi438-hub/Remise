107d2be1373880e77be288b8932d39ebf8688f28
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-08-19 00:38:03 +0100

    s

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+echo "=== DEBUG: checking static folder ==="
+find . -name "style.css" -not -path "*/node_modules/*"
+echo "=== DEBUG: settings STATIC config ==="
+python -c "import django, os; os.environ.setdefault('DJANGO_SETTINGS_MODULE','Remise.settings'); django.setup(); from django.conf import settings; print('STATICFILES_DIRS:', getattr(settings, 'STATICFILES_DIRS', 'NOT SET')); print('INSTALLED_APPS:', settings.INSTALLED_APPS); print('STATIC_ROOT:', settings.STATIC_ROOT)"
 python manage.py collectstatic --no-input
 python manage.py migrate
