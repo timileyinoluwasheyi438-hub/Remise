43da0d52e50a5767954be6b370ee878a54bcd2b2
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-08-19 00:52:42 +0100

    :s:

--- Remise/settings.py
+++ Remise/settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
