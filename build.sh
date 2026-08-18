d22dae8d9ac0d5e3242989155190551ea4ab3276
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-08-19 00:45:24 +0100

    k

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 set -o errexit
 
 pip install -r requirements.txt
-echo "=== DEBUG: checking static folder ==="
-find . -name "style.css" -not -path "*/node_modules/*"
-echo "=== DEBUG: settings STATIC config ==="
-python -c "import django, os; os.environ.setdefault('DJANGO_SETTINGS_MODULE','Remise.settings'); django.setup(); from django.conf import settings; print('STATICFILES_DIRS:', getattr(settings, 'STATICFILES_DIRS', 'NOT SET')); print('INSTALLED_APPS:', settings.INSTALLED_APPS); print('STATIC_ROOT:', settings.STATIC_ROOT)"
+echo "=== DEBUG: checking for duplicate management commands ==="
+find . -iname "*collectstatic*"
 echo "=== DEBUG: finders ==="
 python manage.py findstatic shorp/css/style.css --verbosity 2
-python manage.py collectstatic --no-input
+echo "=== DEBUG: collectstatic verbose ==="
+python manage.py collectstatic --no-input --verbosity 3
 python manage.py migrate
