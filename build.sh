6d19710a117b34d4e9ce88d27a7eba90a8e0faf5
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-08-19 00:49:00 +0100

    ss

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-echo "=== DEBUG: checking for duplicate management commands ==="
-find . -iname "*collectstatic*"
-echo "=== DEBUG: finders ==="
-python manage.py findstatic shorp/css/style.css --verbosity 2
-echo "=== DEBUG: collectstatic verbose ==="
-python manage.py collectstatic --no-input --verbosity 3
+python manage.py collectstatic --no-input
 python manage.py migrate
